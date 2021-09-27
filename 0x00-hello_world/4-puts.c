a3c1f181c82bb88c4bab3af233f95ed7cffb89f7
MontyWilliams <3650@holbertonschool.com>
2021-09-27 11:35:16 -0700

    write a c program that puts a message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
+/**
+ * main - Initiates the function
+ */
 int main (void)
 {
-puts ( "\"Programming is like building a multilingual puzzle" );
-return ( 0 );
+puts ("\"Programming is like building a multilingual puzzle");
+return (0);
 }
