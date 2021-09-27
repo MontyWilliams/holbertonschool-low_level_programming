0ad7b9198cbe683395664b2a2c2c4f7f8e79ae94
MontyWilliams <3650@holbertonschool.com>
2021-09-27 12:27:26 -0700

    C program that prints string using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
-int main(void){
+/*
+ * main - Initiates the function\
+ *
+ * Description: This is the function.
+ * Return: Closes main func
+ */
+int main(void)
+{
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
