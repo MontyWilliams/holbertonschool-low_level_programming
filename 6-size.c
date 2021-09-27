037e66462998fa53aea8aa220f911d2bb2f7df25
MontyWilliams <3650@holbertonschool.com>
2021-09-27 13:59:39 -0600

    check size of 5 diff file types and print to screen

---
+++ 6-size.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - Initiates  function
+ *
+ * Description: The function checks the size of 5 diff file types.
+ * Return: returns 5 values, char, int long int, long long int, and a float
+ */
+int main(void)
+{
+char charType;
+int intType;
+long int lType;
+long long int llType;
+float floatType;
+printf("size of a char: %zu", sizeof(charType));
+printf("size of an int: %zu", sizeof(inType));
+printf("size of a long int: %zu", sizeof(lType));
+printf("size of a long long int: %zu", sizeof(llType));
+printf("size of a float: %zu", sizeof(floatType));
+}
