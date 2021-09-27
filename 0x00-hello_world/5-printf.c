ac6a9e4a5fc9e8ebaf4e7e410b96395701267abb
MontyWilliams <3650@holbertonschool.com>
2021-09-27 12:02:59 -0700

    C program that prints string using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
-char *ptr = "with proper grammar, but the outcome is a piece of art,/n";
+char *ptr = "with proper grammar, but the outcome is a piece of art, ";
 int main(void){
-printf("%s", ptr);
+printf("%s", ptr\n);
 return (0);
 }
