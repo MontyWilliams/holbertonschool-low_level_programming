cca5f32a0232f428fa2273b0d62118c112b2d1e9
MontyWilliams <3650@holbertonschool.com>
2021-09-27 14:08:16 -0700

    print sixe of 4 diff file types using printf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int intType;
 long int lType;
 long long int llType;
 float floatType;
-printf("Size of a char: %zu bytes(s)\n",  sizeof(charType));
-printf("Size of an int: %zu bytes(s)\n", sizeof(intType));
-printf("Size of a long int: %zu bytes(s)\n", sizeof(lType));
-printf("Size of a long long int: %zu bytes(s)\n", sizeof(llType));
-printf("Size of a float: %zu bytes(s)\n", sizeof(floatType));
+printf("Size of a char: %zu byte(s)\n",  sizeof(charType));
+printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+printf("Size of a long int: %zu byte(s)\n", sizeof(lType));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(llType));
+printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 return (0);
 }
