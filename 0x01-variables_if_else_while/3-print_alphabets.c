e597d43a665aab78f5b71f59a6add4f8fc5ef663
MontyWilliams <3650@holbertonschool.com>
2021-09-28 13:45:20 -0700

    use putchr to print alphabet in lower and then upper and add a new line

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Initiates  function
+ *
+ * Description: prints random # and finds last digit
+ * Return: alphabet in lower case followed by upercase and new line
+ */
+int main(void)
+{
+char ch;
+char cg;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar (ch);
+}
+for (cg = 'A'; ch <= 'z'; cg++)
+{
+putchar (cg);
+}
+putchar ('\n');
+return (0);
+}
