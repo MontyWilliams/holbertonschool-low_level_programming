ccf52e102381c3904c3bd173b0fe939ac3ac99b6
MontyWilliams <3650@holbertonschool.com>
2021-09-28 13:27:14 -0700

    use putchr to print alphabet and add a new line

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Initiates  function
+ *
+ * Description: prints random # and finds last digit
+ * Return: returns random # and last digit
+ */
+int main(void)
+{
+char ch;	
+for(ch = 'a'; ch <= 'z'; ch++)
+{
+putchar (ch);
+}
+putchar (ch);
+return (0);
+}
