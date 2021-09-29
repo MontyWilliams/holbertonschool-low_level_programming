b67e000ed738f8da89b216fb4847bd496716cb63
MontyWilliams <3650@holbertonschool.com>
2021-09-29 13:41:30 -0700

    writing islower function

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+ * _islower - checks if a value is lower
+ *
+ * Description: bootleg functions that checks if avalue is lower than int c
+ * Return: 1 if higher 2 if lower
+ */
 int _islower(int c)
 {
 if (c >= 'a' && c <= 'z')
