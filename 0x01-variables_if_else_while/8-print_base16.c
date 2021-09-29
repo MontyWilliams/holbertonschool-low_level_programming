b75aa577f5c6be9ed69313a0c917f101a27206ca
MontyWilliams <3650@holbertonschool.com>
2021-09-28 17:15:11 -0700

    print letters and numbers using modulo

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Initiates  function
+ *
+ * Description: prints alphabet backwards
+ * Return: single char int
+ */
+int main(void)
+{
+int l;
+int n;
+for (l = 0; l < 10; l++)
+{
+putchar ((l % 10) + '0' );
+}
+for (n = 'a'; n < 'g';  n++)
+{
+putchar (n);
+}
+putchar ('\n');
+return (0);
+}
