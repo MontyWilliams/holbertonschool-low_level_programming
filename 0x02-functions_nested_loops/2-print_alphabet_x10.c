fd6cb070f34a44fefcd501ce1768d55ab03b5dfa
MontyWilliams <3650@holbertonschool.com>
2021-09-29 12:40:40 -0700

    header for print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * print_alphabet - header function that prints alphabet
+ * print_alphabet_x10 - header function that prints alphabet
  *
- * Description: using a homemade putchar from main.h
- * Return: returns the alphabet in lower case
+ * Description: using a homemade putchar from main.hand a nested for loop
+ * Return: returns the alphabet in lower case 10x
   */
 void print_alphabet_x10(void)
 {
 int count;
 char i;
-for (count =1; count <= 10; count++)
+for (count = 1; count <= 10; count++)
 {
 for (i = 'a'; i <= 'z'; i++)
 {
