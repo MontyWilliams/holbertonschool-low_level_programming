02d58de46a70eca9dc68d77b19e1857f18945a15
MontyWilliams <3650@holbertonschool.com>
2021-09-29 14:40:17 -0700

    if statements

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,12 +3,12 @@
  * print_sign - checks if a value is lower
  * @n: parmeter for function
  * Description: bootleg functions that checks if avalue is lower than int c
- * Return: 1 
+ * Return: 1
  *
  */
 int print_sign(int n)
 {
-if (n > 0 )
+if (n > 0)
 {
 _putchar ('+');
 return (1);
@@ -20,6 +20,6 @@ return (0);
 }
 else
 _putchar ('-');
-return (0);
+return (-1);
 }
 
