dc25f5cada1e2be26d94ebe601988a0e0e8e49a0
MontyWilliams <3650@holbertonschool.com>
2021-09-29 03:43:57 -0700

    use printf to print output followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
-
+/**
+ * main - Initiates function
+ *
+ *Description: prints a string
+ *Return: 0
+*/
 int main(void)
 {
-printf ("_putchar\n");
+printf("_putchar\n");
 return (0);
 }
