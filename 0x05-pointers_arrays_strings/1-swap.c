ef3efe6f08bd99d3f551d69db599afeb1a1ca5c2
MontyWilliams <3650@holbertonschool.com>
2021-10-05 14:16:09 -0600

    switch values of 2 ints using pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - swaps values of 2 ints
+ * Description; swaos 2
+ * @a: int
+ * @b: int
+ * Return: 2 values
+*/
+void swap_int(int *a, int *b)
+{
+*a = *b;
+*b = *a;
+}
