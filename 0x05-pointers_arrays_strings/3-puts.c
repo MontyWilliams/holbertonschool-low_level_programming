ea327647b25e852b1e1aad283d4a0e0af127d868
MontyWilliams <3650@holbertonschool.com>
2021-10-06 06:20:59 -0600

    erit function that prints a string followed by a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - prints astring
+ * Description:print char string using bootleg _putchar
+ * @str: charaacter string
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+while (*str)
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+}
