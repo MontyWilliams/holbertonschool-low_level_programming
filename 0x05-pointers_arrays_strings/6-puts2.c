2f3d5a616efa784aec83ad332ec6b1dde329034a
MontyWilliams <3650@holbertonschool.com>
2021-10-06 13:55:20 -0600

    print every other element in a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts2 - prints a string
+ * Description:print char string and skip every other element in array
+ * @str: charaacter string
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+int i = 0;
+
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+i++;
+if (str[i] == '\0')
+i++;
+}
+_putchar('\n');
+}
