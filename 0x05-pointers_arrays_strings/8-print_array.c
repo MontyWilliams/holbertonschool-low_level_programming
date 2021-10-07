7982495f27df33c8f1023893162d4e1acb1f4e21
MontyWilliams <3650@holbertonschool.com>
2021-10-07 15:45:05 -0600

    write a function thst tskes a pointer and print an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints an array
+ * @a: pointer int
+ * @n: int
+ * Description: print an array
+ * Return: Always 0.
+ */
+void print_array(int *a, int n)
+{
+int i;
+while (i < n)
+{
+if (i != n - 1)
+printf("%d, ", a[i]);
+else
+printf("%d", a[i]);
+i++;
+}
+putchar('\n');
+}
