0c0423afee31f7f94e762db960f74d4688647f31
MontyWilliams <3650@holbertonschool.com>
2021-10-07 22:03:21 -0600

    print diagnal lines using nested for loop

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,12 +7,17 @@
  */
 void print_diagonal(int n)
 {
+int j;
 int i;
-if (n > 0)
-{
+if (n >= 0)
+_putchar('\n');
 for (i = 0; i < n; i++)
+{
+for (j = 0; j < i; j++)
+{
 _putchar(' ');
 }
 _putchar('\\');
 _putchar('\n');
+}
 }
