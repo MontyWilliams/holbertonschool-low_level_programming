ada2264460a32825207cf55e1b16d8507f4749a2
MontyWilliams <3650@holbertonschool.com>
2021-10-10 22:13:05 -0600

    capitolize every word in a string but skip certain cahers

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,26 +7,27 @@
  */
 char *cap_string(char *str)
 {
-int i, j;
-char sep[] = " \t\n,;.!?\"(){}";
+	int i;
 
-	for (i = 0; str[i] != 0; i++)
-	{
+	i = 0;
+
 	while (str[i] != '\0')
 	{
-		for (j = 0; sep[j] != '\0'; j++)
-			if (str[i - 1] == sep[j])
-				{
-					i++;
-				}
-
 		if (str[i] >= 'a' && str[i] <= 'z')
-	{
-		str[i] -= 32;
-	}
+		{
+			if (str[i - 1] == ' ' || str[i - 1] == '\n' ||
+			str[i - 1] == '\t' || str[i - 1] == '.' ||
+			str[i - 1] == ',' || str[i - 1] == ';'
+			|| str[i - 1] == '!' || str[i - 1] == '?'
+			|| str[i - 1] == '"' || str[i - 1] == '('
+			|| str[i - 1] == ')' || str[i - 1] == '{'
+			|| str[i - 1] == '}')
+			{
+				str[i] = str[i] - 32;
+			}
+		}
 
+	i++;
 	}
-		i++;
-	}
-return (str);
+	return (str);
 }
