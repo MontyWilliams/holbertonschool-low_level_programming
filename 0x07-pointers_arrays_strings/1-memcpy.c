0b00265a4ef769a9cdf5b03eaff2745cf0c01243
MontyWilliams <3650@holbertonschool.com>
2021-10-11 14:17:15 -0600

    copy a memory are to another file

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,18 +1,20 @@
 #include "main.h"
 /**
- * _mempcpy - fill memory with a constant byte
+ * _memcpy - fill memory with a constant byte
  * @dest: pointer to file to be filled
  * @src: source file
  * @n: number of iterations
- * Description: fills the first n bytes of memory area pointed to by n
+ * Description: copires segment of memory
  * Return: void
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
+
 {
 
 	unsigned int i;
 	char *t = (char *)dest;
 	char *s = (char *)src;
+
 	for (i = 0; i < n; i++)
 		t[i] = s[i];
 
