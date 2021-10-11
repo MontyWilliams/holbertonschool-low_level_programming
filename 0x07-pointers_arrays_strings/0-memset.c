ffbb5766cebe09de34a947f2ab58e55a5e9602a6
MontyWilliams <3650@holbertonschool.com>
2021-10-11 13:23:22 -0600

    copies a value into the first (n) chars of a line

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _memset - fill memory with a constant byte
+ * @s: pointer to file to be filled
+ * @b: char to be printed
+ * @n: number of iterations
+ * Description: fills the first n bytes of memory area pointed to by n
+ * Return: void
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *dst = s;
+
+	while (n > 0)
+	{
+		*dst = (unsigned char) b;
+		dst++;
+		n--;
+	}
+	return (s);
+}
