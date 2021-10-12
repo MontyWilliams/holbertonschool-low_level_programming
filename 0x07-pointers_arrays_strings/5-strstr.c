f0acde9b71d334684363f03d173c4bf3b40b9f17
MontyWilliams <3650@holbertonschool.com>
2021-10-11 22:40:49 -0600

    find substring within a string

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strstr - find a substring within a string
+ * Description: search
+ * @haystack: string to search within
+ * @needle: search parameters
+ * Return: chars found or null
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *a = haystack, *b = needle;
+		for (;;)
+		{
+		if (!*b)
+			return ((char *) haystack);
+		if (!*a)
+			return (0);
+		if (*a++ == *b++)
+			continue;
+			a = ++haystack;
+			b = needle;
+		}
+
+}
