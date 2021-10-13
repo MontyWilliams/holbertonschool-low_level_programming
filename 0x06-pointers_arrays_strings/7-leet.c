e5db7421361207f1d10fa97a1cf68fdb26ef8671
MontyWilliams <3650@holbertonschool.com>
2021-10-13 14:26:00 -0600

    encode a string

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,13 +1,32 @@
-#include "main.h"
-
+#include "holberton.h"
+#include <stdio.h>
 /**
  * leet - encodes a string
- * Description: encod strng
+ * @str: string to encode
+ * Description: encode strng
  * Return: Always 0.
  */
-char *leet(char *s)
+char *leet(char *str)
 {
+	int x = 0;
+	int cc;
+	char thischar;
 
+	char la[] = "aeotl";
+	char ua[] = "AEOTL";
+	char lt[] = "43071";
 
-return (s);
+	while (*(str + x) != '\0')
+	{
+		thischar = *(str + x);
+		for (cc = 0; cc < 5; cc++)
+		{
+			if (la[cc] == thischar || ua[cc] == thischar)
+				*(str + x) = lt[cc];
+		}
+
+		x++;
+	}
+
+	return (str);
 }
