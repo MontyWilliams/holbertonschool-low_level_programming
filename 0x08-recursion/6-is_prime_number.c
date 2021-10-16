38c135008233ee2c90a370226392bf09cbb1da8d
MontyWilliams <3650@holbertonschool.com>
2021-10-15 19:54:46 -0600

    find prime # recursively

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * is_prime_number - finds prime #
+ * @n: number to calculate
+ * Description: finds prime #
+ * Return: 0
+ */
+int is_prime_number(int n)
+{
+return (n);
+
+}
