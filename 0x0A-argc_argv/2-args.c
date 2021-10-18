bf34fa6616b22a9458986ccce643478c047700a1
MontyWilliams <3650@holbertonschool.com>
2021-10-17 19:09:29 -0600

    print the # of arguments recieved

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * @argc: counts # of arguments
+ * @argv: arguments
+ * Description:  prints all arguments on a new line
+ * Return: 0
+ */
+
+
+int main(int argc, char  *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+	
+		i++;
+	}
+	return (0);
+}
