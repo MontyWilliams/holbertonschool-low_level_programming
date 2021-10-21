90054436a58267fac341c00321ea1ad1c108ffe1
MontyWilliams <3650@holbertonschool.com>
2021-10-21 12:45:54 -0600

    create a macro that finds absolute value

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,5 @@
-#ifndef FUNCTION_LIKE_MACRO
-#define FUNCTION_LIKE_MACRO
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 #define SIZE 1024
 #define PI 3.14159265359
 #define ABS(x) ((x < 0) ? (-x) : (x))
