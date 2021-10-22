23bb72170a6980f9353d4b9dd1771d1b2ee5abd8
MontyWilliams <3650@holbertonschool.com>
2021-10-22 10:38:07 -0600

    alloc space for a 2d array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid  - prints a grid of integers
+ * @width: width of the grid
+ * @height: height of the grid
+ * Description: alloc 2d array
+ * Return: Nothing.
+ */
+int **alloc_grid(int width, int height)
+{
+
+int i;
+int **ptr;
+ptr = malloc(sizeof(int *) * height);
+
+if (!ptr)
+return (NULL);
+
+for (i = 0; i < height; i++)
+ptr[i] = malloc(sizeof(int) * width);
+return (ptr);
+}
