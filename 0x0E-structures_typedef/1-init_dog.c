58fb6b000412f4422dd69ee3616a4949764abe8e
MontyWilliams <3650@holbertonschool.com>
2021-10-26 13:10:03 -0600

    initiate an instance of a dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - woof woof
+ * @d: dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Description: Bow Wow
+ * Return: Always 0.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
