f0958358967ae2d7db12aef611af76e0365fb726
MontyWilliams <3650@holbertonschool.com>
2021-10-25 22:45:43 -0600

    array range

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * array_range - creates an array of integers from min to max
+ * @min: int
+ * @max: int
+ *Description: create an array of ints
+ * Return: ptr to array
+ */
+void simple_print_buffer(int *buffer, unsigned int size)
+{
+
+}
