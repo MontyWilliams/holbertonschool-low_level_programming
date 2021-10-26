314c57ab028d233606065b94d957e5f60b679dec
MontyWilliams <3650@holbertonschool.com>
2021-10-26 13:32:53 -0600

    header files

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -1,20 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-/**
- * struct dog - struct about a dog
- * @name: dog name
- * @age: dog age
- * @owner: dog owner
- *
- */
-
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
-
-
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif
