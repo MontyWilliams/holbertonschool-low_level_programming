3ba9d71c4e92bcb4856ad50a44c20e00dcba41cc
MontyWilliams <3650@holbertonschool.com>
2021-10-27 22:55:23 -0600

    write a program that performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,12 +15,20 @@ int main(int argc, char *argv[])
 	int a;
 	int b;
 	int (*ans)(int, int);
+	char *valid = "+-*/%";
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+
+	if (!strstr(valid, argv[2]))
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 	ans = get_op_func(argv[2]);
