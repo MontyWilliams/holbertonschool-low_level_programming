1ed2b84f04f753a99be3954d3542225a32db6fed
MontyWilliams <3650@holbertonschool.com>
2021-10-28 12:53:28 -0600

    create main and input function

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -46,11 +46,6 @@
  */
 	int op_div(int a, int b)
 	{
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	return (a / b);
 	}
 
@@ -62,10 +57,5 @@
  */
 	int op_mod(int a, int b)
 	{
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	return (a % b);
 	}
