87d67dad9f53406c816e1290df56ce9184bcdbb4
MontyWilliams <3650@holbertonschool.com>
2021-10-28 12:53:28 -0600

    create main and input function

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,14 +18,15 @@ int (*get_op_func(char *s))(int, int)
 		{NULL, NULL}
 	};
 
-	int i;
+	int i = 0;
 
-	for (i = 0; i < 5; i++)
+	while (i < 5)
 	{
 		if (*ops[i].op == *s)
 		{
 			return (ops[i].f);
 		}
+	i++;
 	}
 	return (NULL);
 
