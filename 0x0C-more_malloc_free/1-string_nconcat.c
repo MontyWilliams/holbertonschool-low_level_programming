e838c1795aaf19cc0baa0510ffd6a65cfde433cc
MontyWilliams <3650@holbertonschool.com>
2021-10-28 07:21:21 -0600

    concat string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,51 +4,48 @@
 #include <string.h>
 
 /**
- * malloc_checked - allocat memory
- * @b: int
+ * string_nconcat - allocat memory
+ * @s1: char
+ * @s2: char
+ * @n: int
  * Description: check and allocate memory
  * Return: Always 0.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *ptr;
-    unsigned int i, s1len = strlen(s1), s2len = strlen(s2);
-    unsigned int j = 0;
+	char *ptr;
+	unsigned int i, s1len = strlen(s1), s2len = strlen(s2);
+	unsigned int j = 0;
 
-    if (!s1)
-        s1 = "";
+	if (!s1)
+	s1 = "";
 
-    if (!s1)
-        s1 = "";
+	if (!s1)
+	s1 = "";
 
-    
+	ptr = malloc(sizeof(*ptr) * s1len + s2len + 1);
 
-    ptr = malloc (sizeof(*ptr) * s1len + s2len + 1);
+	if (!ptr)
+	return (NULL);
 
-    if (!ptr)
-    return (NULL);
-    
-    for (i = 0; i < s1len ; i++)
-    {
-        ptr[i] = s1[i];
-    }
+	for (i = 0; i < s1len ; i++)
+	{
+	ptr[i] = s1[i];
+	}
 
-      while (j < n)
-      {
-      ptr[i] = s2[j];
-      ptr++;
-      j++;
-      }
-      while (n >= s2len)
-      {
-      ptr[i] = s2[j];
-      ptr++;
-      j++;
-       if (n < (s1len + s2len + 1))
-       break;
-      }
-      
-   
-    
-    return (ptr);
+	while (j < n)
+	{
+	ptr[i] = s2[j];
+	ptr++;
+	j++;
+	}
+	while (n >= s2len)
+	{
+	ptr[i] = s2[j];
+	ptr++;
+	j++;
+	if (n < (s1len + s2len + 1))
+	break;
+	}
+return (ptr);
 }
