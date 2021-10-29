d1d25bcd107bfb60fc268b99c4ebeb0864b7611b
MontyWilliams <3650@holbertonschool.com>
2021-10-29 03:44:57 -0600

    write a function that adds 2 strings

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - adds sum of all parameters
+ * @n: integer being added
+ * Description: adds however many #'s u give it
+ * Return: sum of ints
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum;
+
+	va_list ap;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+
+	va_end(ap);
+	if (n == 0)
+		return (0);
+
+	return (sum);
+}
