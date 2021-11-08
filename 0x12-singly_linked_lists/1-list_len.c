187f62c573734a8198b3c6e2b385884f32470927
MontyWilliams <3650@holbertonschool.com>
2021-11-08 16:45:57 -0600

    make a function that returns the number of elements in a linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * list_len - list length singl link list
+ * @h: linked list element
+ * Return: number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	const list_t *temp;
+	int n = 0;
+
+	temp = h;
+	do {
+		temp = temp->next;
+		n++;
+		} while (temp != NULL);
+	return (n);
+}
