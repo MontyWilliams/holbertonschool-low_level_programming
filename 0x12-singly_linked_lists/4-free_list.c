5425977a47ccc9a47c3af1c824f2fdffd8720d88
MontyWilliams <3650@holbertonschool.com>
2021-11-08 19:18:59 -0600

    free singly linked list elements

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_list - free elements
+ * @head: list
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *h;
+
+	while (head)
+	{
+		h = head;
+		head = head->next;
+		free(h->str);
+
+	}
+
+}
