0b8142863fcfd79b728246ed0a14ab785659e240
MontyWilliams <3650@holbertonschool.com>
2021-11-11 07:08:02 -0600

    prints the elements of int linked lists on a new line followed by tottal # of elements

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * print_listint - print linked lists of ints
+ * @h: head ptr
+ * Description: i put the whole logic in a for loop
+ * Return: count var which is size_t
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count;
+
+	for (count = 0; (h); h = h->next, count++)
+		printf("%d\n",  h->n);
+			return (count);
+}
