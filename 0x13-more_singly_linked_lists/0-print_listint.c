cc2b608db4204bcaf13bc3772d703c54487a3bb6
MontyWilliams <3650@holbertonschool.com>
2021-11-11 07:46:36 -0600

    prints the elements of int linked lists on a new line

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,7 +9,7 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count;
 
-	for (count = 0; (h); h = h->next, count++)
+	for (count = 0; (h); h = (h->next), count++)
 		{
 			_putchar((h->n) + '0');
 			_putchar('\n');
