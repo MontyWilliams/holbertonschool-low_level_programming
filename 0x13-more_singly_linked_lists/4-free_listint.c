8c6735a399fdf8bdd98aa7e1c2386b17f5540667
MontyWilliams <3650@holbertonschool.com>
2021-11-11 21:49:25 -0600

    free lists

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - free listint_t list
+ * @head: pointer to head node
+ * Return: void
+ */
+
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
