e0b0534b344fab1cf0669e55ac803303ff3c4787
MontyWilliams <3650@holbertonschool.com>
2021-11-12 19:27:53 -0600

    convert binary to int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary to unsigned int
+ * @b: input
+ * Return: uint
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i = 0, sum;
+
+	if (!b)
+		return (0);
+
+	while (b[i])
+	{
+		if (b[i] == '1' || b[i] == '0')
+		{
+			sum = ((sum * 2) + (b[i] - '0'));
+		}
+		else
+		{
+			return (0);
+		}
+		i++;
+	}
+return (sum);
+}
