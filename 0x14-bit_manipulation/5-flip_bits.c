bf9a73bb997c55f6af022e2c681ed196df1d3910
MontyWilliams <3650@holbertonschool.com>
2021-11-12 23:35:31 -0600

    function should flip bits. its dosnt work tho..

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * flip_bits - finds the number flips
+ * @n: input var
+ * @m: input var
+ * Return: uint
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	
+return (flips);
+}
