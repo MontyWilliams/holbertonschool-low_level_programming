3c1153a3ff02562bdd12d939ddfc63539003f748
MontyWilliams <3650@holbertonschool.com>
2021-11-15 14:37:43 -0600

    return the number of flips required to turn n into m

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,6 +10,9 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned int count = 0;
 	unsigned int xor = n ^ m;
 
+	if (!n || !m)
+		return (0);
+
 	while (xor > 0)
 	{
 		count++;
