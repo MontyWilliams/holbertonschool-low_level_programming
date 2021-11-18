51046a9d1ec2768575e38bda2e3725d135d4871f
MontyWilliams <3650@holbertonschool.com>
2021-11-17 21:49:31 -0600

    create a file and writ contents or exit if it already exists

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,12 +18,10 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	while (text_content[i])
-	{
-		i++;
-	}
+	for (i = 0; text_content[i] != '\0'; i++)
+		;
 		write(fd, text_content, i);
 
-	close(fd);
+		close(fd);
 	return (1);
 }
