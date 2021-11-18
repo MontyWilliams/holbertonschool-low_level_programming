c57a61627847478867d4424d45e6bcf08c922f4a
MontyWilliams <3650@holbertonschool.com>
2021-11-17 21:49:31 -0600

    create a file and writ contents or exit if it already exists

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file and writes to it or modifies existing file
+ * @filename: name of file
+ * @text_content: content to write
+ * Return: 1 on success -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i = 0;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_APPEND | O_CREAT | O_WRONLY, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	while (text_content[i])
+	{
+		i++;
+	}
+		write(fd, text_content, i);
+
+	close(fd);
+	return (1);
+}
