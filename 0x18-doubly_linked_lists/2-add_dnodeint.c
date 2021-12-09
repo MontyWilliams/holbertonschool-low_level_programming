313a81ebecee9ea29470dda60e0c412a04e0ac96
MontyWilliams <3650@holbertonschool.com>
2021-12-08 21:06:52 -0600

    add mode to the beggining of the list

---
+++ 0x18-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - add node to beggining of list
+ * @head: pointer
+ * @n: Value to add
+ * Return: New node
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+	new->prev = NULL;
+	if (*head != NULL)
+		(*head)->prev = new;
+	*head = new;
+
+	return (new);
+}
