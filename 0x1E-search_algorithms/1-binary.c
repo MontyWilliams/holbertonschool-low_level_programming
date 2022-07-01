cf0dfe0c45b086f176791e1eb3647746d9244393
Montywilliams <3650@holbertonschool.com>
2022-06-30 22:45:07 -0600

    not working

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#incl
+ude "search_algos.h"
+
+/**
+ * binary_search - binary search algo
+ * @array: array of
+ * @value: value to search
+ * @size: size of
+ * description: search bru bru
+ * Return: Always EXIT_SUCCESS
+ */
+
+int binary_search(int *array, size_t size, int value);
+{
+  int left = size[0];
+  int right = (size - 1);
+  int mid = (left + right) / 2;
+
+  c = (a < b) ? a : b;                        +
+
+}
